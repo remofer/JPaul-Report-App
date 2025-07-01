c2c0e89a1ef55197e87f47f90174550883ad6490
Remo Villarreal <remofer98@gmail.com>
2025-06-30 21:44:11 -0300

    isEmbeddedApp: true,

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -14,6 +14,7 @@ const shopify = shopifyApp({
   scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
+  isEmbeddedApp: true,
   hostName: process.env.HOST,
   useOnlineTokens: true,
   sessionStorage: new PrismaSessionStorage(prisma),
